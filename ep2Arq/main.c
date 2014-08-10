1d2c8745e0a88e8710ae99e482f4ad0f5339dc72
Tiago Bernardinelli <tiago.bernardinelli@gmail.com>
2014-08-10 17:59:20 -0300

    quick paralelo

--- ep2Arq/main.c
+++ ep2Arq/main.c
@@ -3,6 +3,14 @@
 #include <dirent.h>
 #include <string.h>
 #include <time.h>
+#include <omp.h>
+#include <math.h>
+#define VERBOSE 0
+
+static int CmpInt(const void *a, const void *b)
+{
+	return (*(int*)a - *(int*)b);
+}
 
 #define troca( A, B) { int t = A; A = B; B = t; } 
 void quicksort(int v[], int p, int r) {
@@ -20,13 +28,67 @@ void quicksort(int v[], int p, int r) {
 	}
 }
 
-int ordena(int tam, int tipo, int* vetor){
+/* Merge sorted lists A and B into list A.  A must have dim >= m+n */
+void merge(int A[], int B[], int m, int n)
+{
+	int i = 0, j = 0, k = 0;
+	int size = m + n;
+	int *C = (int *)malloc(size*sizeof(int));
+	while (i < m && j < n) {
+		if (A[i] <= B[j]) C[k] = A[i++];
+		else C[k] = B[j++];
+		k++;
+	}
+	if (i < m) for (int p = i; p < m; p++, k++) C[k] = A[p];
+	else for (int p = j; p < n; p++, k++) C[k] = B[p];
+	for (i = 0; i<size; i++) A[i] = C[i];
+	free(C);
+}
+
+/* Merges N sorted sub-sections of array a into final, fully sorted array a */
+void arraymerge(int *a, int size, int *index, int N)
+{
+	int i;
+	while (N>1) {
+		for (i = 0; i<N; i++) index[i] = i*size / N; index[N] = size;
+#pragma omp parallel for private(i) 
+		for (i = 0; i<N; i += 2) {
+			if (VERBOSE) fprintf(stderr, "merging %d and %d, index %d and %d (up to %d)\n", i, i + 1, index[i], index[i + 1], index[i + 2]);
+			merge(a + index[i], a + index[i + 1], index[i + 1] - index[i], index[i + 2] - index[i + 1]);
+			if (VERBOSE) for (int i = 0; i<size; i++) fprintf(stderr, "after: %d %d\n", i, a[i]);
+		}
+		N /= 2;
+	}
+}
+
+int ordena(int tam, int tipo, int* vetor, int* vetorparalelo){
 	if (tipo == 1){
+
+		/*PARALELO*/
+		int threads = 2;
+		int *index = (int *)malloc((threads + 1)*sizeof(int));
+		for (int i = 0; i<threads; i++) 
+			index[i] = i*tam / threads; 
+		index[threads] = tam;
+
+		double start = omp_get_wtime();
+		
+#pragma omp parallel for private(i)
+		for (int i = 0; i<threads; i++) 
+			qsort(vetorparalelo + index[i], index[i + 1] - index[i], sizeof(int), CmpInt);
+		double middle = omp_get_wtime();
+		/* Merge sorted array pieces */
+		if (threads>1) arraymerge(vetorparalelo, tam, index, threads);
+		double end = omp_get_wtime();
+		fprintf(stderr, "\n\t sort time = %g s, of which %g s is merge time ", end - start, end - middle);
+
+		/*SERIAL*/		
 		clock_t startclock = clock();
 		quicksort(vetor, 0, tam - 1);
 		double d = clock() - startclock, msec;
 		msec = d;
-		printf(":%f milissegundos", msec);
+
+		printf("\n\tserial:%f ms", msec);
 		return tam;
 	}
 	else {
@@ -37,6 +99,14 @@ int ordena(int tam, int tipo, int* vetor){
 
 int main()
 {
+	SYSTEM_INFO sysinfo;
+	GetSystemInfo(&sysinfo);
+
+	int threads = 2; //omp_get_max_threads();
+	omp_set_num_threads(threads);
+
+	printf("Number Of Processors %d \n", sysinfo.dwNumberOfProcessors, sysinfo.dwProcessorType, sysinfo.wProcessorArchitecture);
+
 	char * directoryName = "Dados_Ep2";
 	dirent * dp;
 	DIR * dir = opendir(directoryName);
@@ -52,7 +122,7 @@ int main()
 				sprintf(fileLenght, "%.*s", dp->d_namlen - 3 -4, &fileName[3]);
 				int fileLenghtI;
 				sscanf(fileLenght, "%d", &fileLenghtI);
-				printf("\n %s ",fileToOpen);
+				printf("\n %s ",dp->d_name);
 				
 				/*char * word;
 				scanf("%s", &word);*/
@@ -61,11 +131,13 @@ int main()
 				if (file != NULL)
 				{
 					int * vector = malloc(fileLenghtI * sizeof(int));
+					int * vectorParallel = malloc(fileLenghtI * sizeof(int));
 					int cur;
 					int i = 0;
 					do {
 						cur = fgetc(file);						
 						vector[i] = cur;
+						vectorParallel[i] = cur;
 						i++;
 					} while (i < fileLenghtI);
 					fclose(file);					
@@ -75,16 +147,19 @@ int main()
 					//	printf("%i,", vector[i]);
 					//printf("\n\n");
 					
-					ordena(fileLenghtI, 1, vector);
+					ordena(fileLenghtI, 1, vector, vectorParallel);
 
-					//printf("\nVetor processado: ");
-					//for (i = 0; i < fileLenghtI; i++)
-					//	printf("%i,", vector[i]);
-					//printf("\n\n");
+			/*		printf("\nVetor paralelo processado: ");
+					for (i = 0; i < fileLenghtI; i++)
+						printf("%i,", vectorParallel[i]);
+					printf("\n\n");*/
 					
 					//processa.
 					free(vector);
 					vector = NULL;
+
+					free(vectorParallel);
+					vectorParallel = NULL;
 				}
 				else
 					printf("\nArquivo não encontrado");
