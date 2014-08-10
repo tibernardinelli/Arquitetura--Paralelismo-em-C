0e3bbe321f4c829e634691c68acd0d9e4cf356bc
Tiago Bernardinelli <tiago.bernardinelli@gmail.com>
2014-08-10 12:53:14 -0300

    leitura de arquivos completa.

--- ep2Arq/main.c
+++ ep2Arq/main.c
@@ -7,7 +7,7 @@
 
 int main()
 {
-	char * directoryName = "c://ep2//";
+	char * directoryName = "Dados_Ep2";
 	dirent * dp;
 	DIR * dir = opendir(directoryName);
 	while (dir){
@@ -16,19 +16,40 @@ int main()
 			if (dp->d_namlen > 2){
 				char * fileName = dp->d_name;
 				char fileToOpen[80];
-				sprintf(fileToOpen,"%s%s", directoryName, fileName);
+				sprintf(fileToOpen,"%s//%s", directoryName, fileName);
 
 				char fileLenght[80];
 				sprintf(fileLenght, "%.*s", dp->d_namlen - 3 -4, &fileName[3]);
-					
-				printf("Arquivo a processar: %s, numeros: %s \n",fileToOpen, fileLenght);
+				int fileLenghtI;
+				sscanf(fileLenght, "%d", &fileLenghtI);
+				printf("\nArquivo a processar: %s, numeros: %s \n",fileToOpen, fileLenght);
 				
+				char * word;
+				scanf("%s", &word);
 
 				FILE* file = fopen(fileToOpen, "r");
 				if (file != NULL)
 				{
-					
+					int * vector = malloc(fileLenghtI * sizeof(int));
+					int cur;
+					int i = 0;
+					do {
+						cur = fgetc(file);						
+						vector[i] = cur;
+						i++;
+					} while (i < fileLenghtI);
 					fclose(file);					
+
+					printf("\nVetor inicial: ");
+					for (i = 0; i < fileLenghtI; i++)
+						printf("%i,", vector[i]);
+					printf("\n\n");
+					
+					
+					
+					//processa.
+					free(vector);
+					vector = NULL;
 				}
 				else
 					printf("Arquivo não encontrado");
@@ -39,11 +60,6 @@ int main()
 		
 	}
 	closedir(dir);
-	
-
-
-
-	
 
 	printf("Works!");
 	char * word;
@@ -51,6 +67,12 @@ int main()
 	return EXIT_SUCCESS;
 }
 
-int quickSortSerial(int tam, int tipo, int* vetor){
-
+int ordena(int tam, int tipo, int* vetor){
+	if (tipo == 1){
+		quicksort(vetor, 0, tam);
+		return tam;
+	} 
+	else {
+		return sizeof(vetor)/sizeof(int);
+	}
 }
