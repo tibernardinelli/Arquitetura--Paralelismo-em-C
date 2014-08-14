a3c1f84203d90ca6aff6a199427332554d885634
Tiago Bernardinelli <tiago.bernardinelli@gmail.com>
2014-08-14 01:40:47 -0300

    resultados

--- ep2Arq/main.c
+++ ep2Arq/main.c
@@ -6,6 +6,7 @@
 #include <omp.h>
 #include <math.h>
 #define VERBOSE 0
+#define THREADS 4
 
 static int CmpInt(const void *a, const void *b)
 {
@@ -41,7 +42,7 @@ void merge(int A[], int B[], int m, int n)
 	}
 	if (i < m) for (int p = i; p < m; p++, k++) C[k] = A[p];
 	else for (int p = j; p < n; p++, k++) C[k] = B[p];
-	for (i = 0; i<size; i++) A[i] = C[i];
+	for (i = 0; i < size; i++) A[i] = C[i];
 	free(C);
 }
 
@@ -49,50 +50,138 @@ void merge(int A[], int B[], int m, int n)
 void arraymerge(int *a, int size, int *index, int N)
 {
 	int i;
-	while (N>1) {
-		for (i = 0; i<N; i++) index[i] = i*size / N; index[N] = size;
+	while (N > 1) {
+		for (i = 0; i < N; i++) index[i] = i*size / N; index[N] = size;
 #pragma omp parallel for private(i) 
-		for (i = 0; i<N; i += 2) {
+		for (i = 0; i < N; i += 2) {
 			if (VERBOSE) fprintf(stderr, "merging %d and %d, index %d and %d (up to %d)\n", i, i + 1, index[i], index[i + 1], index[i + 2]);
 			merge(a + index[i], a + index[i + 1], index[i + 1] - index[i], index[i + 2] - index[i + 1]);
-			if (VERBOSE) for (int i = 0; i<size; i++) fprintf(stderr, "after: %d %d\n", i, a[i]);
+			if (VERBOSE) for (int i = 0; i < size; i++) fprintf(stderr, "after: %d %d\n", i, a[i]);
 		}
 		N /= 2;
 	}
 }
 
-int ordena(int tam, int tipo, int* vetor, int* vetorparalelo){
-	if (tipo == 1){
+int quickSortParalelo(int tam, int * vetorparalelo)
+{
+	/*PARALELO*/
+	int threads = THREADS;
+	int *index = (int *)malloc((threads + 1)*sizeof(int));
+	for (int i = 0; i < threads; i++)
+		index[i] = i*tam / threads;
+	index[threads] = tam;
 
-		/*PARALELO*/
-		int threads = 2;
-		int *index = (int *)malloc((threads + 1)*sizeof(int));
-		for (int i = 0; i<threads; i++) 
-			index[i] = i*tam / threads; 
-		index[threads] = tam;
-
-		double start = omp_get_wtime();
-		
+	int i;
 #pragma omp parallel for private(i)
-		for (int i = 0; i<threads; i++) 
-			qsort(vetorparalelo + index[i], index[i + 1] - index[i], sizeof(int), CmpInt);
-		double middle = omp_get_wtime();
-		/* Merge sorted array pieces */
-		if (threads>1) arraymerge(vetorparalelo, tam, index, threads);
-		double end = omp_get_wtime();
-		fprintf(stderr, "\n\t sort time = %g s, of which %g s is merge time ", end - start, end - middle);
+	for (i = 0; i < threads; i++)
+		qsort(vetorparalelo + index[i], index[i + 1] - index[i], sizeof(int), CmpInt);
 
-		/*SERIAL*/		
-		clock_t startclock = clock();
-		quicksort(vetor, 0, tam - 1);
-		double d = clock() - startclock, msec;
-		msec = d;
+	/* Merge sorted array pieces */
+	if (threads>1) arraymerge(vetorparalelo, tam, index, threads);
 
-		printf("\n\tserial:%f ms", msec);
+}
+
+void adiciona(int valor, int tam, int ** vetor){
+	int * novoVetor = malloc(tam * sizeof(int));
+	for (int i = 0; i < tam - 1; i++)
+		novoVetor[i] = (*vetor)[i];
+	novoVetor[tam - 1] = valor;
+	int * temp = *vetor;
+	*vetor = novoVetor;
+	free(temp);
+}
+
+int ordenaPorEliminacao(int tam, int ** vetor){
+	int aComparar = -1;
+	int * novoVetor = malloc(sizeof(int));
+	int contador = 0;
+	for (int i = 0; i < tam; i++)
+	{
+		if ((*vetor)[i] > aComparar){
+			aComparar = (*vetor)[i];
+			contador++;
+			adiciona(aComparar, contador, &novoVetor);
+		}
+	}
+	int * temp = *vetor;
+	*vetor = novoVetor;
+	free(temp);
+	return contador;
+}
+
+int ordenaPorEliminacaoParalela(int tam, int ** vetor){
+	int * novoVetor = malloc(sizeof(int));
+	int contador = 0;
+	int i;
+#pragma omp parallel
+	{
+		int aComparar = -1;
+#pragma omp for 
+		for (i = 0; i < tam; i++){
+			if (aComparar == -1)
+				aComparar = (*vetor)[i];
+			if (aComparar >(*vetor)[i])
+			{
+				aComparar = (*vetor)[i];
+#pragma omp critical 
+				{
+					contador++;
+					adiciona(aComparar, contador, &novoVetor);					
+				}
+			}
+		}
+	}
+
+	 int resultado = ordenaPorEliminacao(contador, &novoVetor);
+
+	int * temp = *vetor;
+	*vetor = novoVetor;
+	free(temp);
+	return resultado;
+}
+
+int ordena(int tam, int tipo, int ** vetor){
+	int* vetorParalelo = malloc(tam * sizeof(int));
+	for (int i = 0; i < tam; i++)
+		vetorParalelo[i] = (*vetor)[i];
+	char filename[100];
+	sprintf(filename, "resultado_tam%i_tipo%i.txt", tam, tipo);
+
+	FILE * resultado = fopen(filename, "w");
+
+	if (tipo == 1){
+			{
+				double start = omp_get_wtime();
+				quickSortParalelo(tam, vetorParalelo);
+				double end = omp_get_wtime();
+				fprintf(resultado, "\n\t tempo qs em paralelo %g", end - start);
+			}
+		{
+			double start = omp_get_wtime();
+			quicksort((*vetor), 0, tam - 1);
+			double end = omp_get_wtime();
+			fprintf(resultado, "\n\t tempo qs em serial %g", end - start);
+		}
+		fclose(resultado);
 		return tam;
 	}
 	else {
-		return sizeof(vetor) / sizeof(int);
+		int retorno;
+		{
+			double start = omp_get_wtime();
+			retorno = ordenaPorEliminacao(tam, vetor);
+			double end = omp_get_wtime();
+			fprintf(resultado, "\n\t tempo elimin em serial %g", end - start);
+		}
+			{
+				double start = omp_get_wtime();
+				retorno = ordenaPorEliminacaoParalela(tam, &vetorParalelo);
+				double end = omp_get_wtime();
+				fprintf(resultado, "\n\t tempo elimin em paralelo %g", end - start);
+			}
+
+		fclose(resultado);
+		return retorno;
 	}
 }
 
@@ -102,8 +191,7 @@ int main()
 	SYSTEM_INFO sysinfo;
 	GetSystemInfo(&sysinfo);
 
-	int threads = 2; //omp_get_max_threads();
-	omp_set_num_threads(threads);
+	omp_set_num_threads(THREADS);
 
 	printf("Number Of Processors %d \n", sysinfo.dwNumberOfProcessors, sysinfo.dwProcessorType, sysinfo.wProcessorArchitecture);
 
@@ -116,64 +204,60 @@ int main()
 			if (dp->d_namlen > 2){
 				char * fileName = dp->d_name;
 				char fileToOpen[80];
-				sprintf(fileToOpen,"%s//%s", directoryName, fileName);
+				sprintf(fileToOpen, "%s//%s", directoryName, fileName);
 
 				char fileLenght[80];
-				sprintf(fileLenght, "%.*s", dp->d_namlen - 3 -4, &fileName[3]);
+				sprintf(fileLenght, "%.*s", dp->d_namlen - 3 - 4, &fileName[3]);
 				int fileLenghtI;
 				sscanf(fileLenght, "%d", &fileLenghtI);
-				printf("\n %s ",dp->d_name);
-				
+				printf("\n %s ", dp->d_name);
+
 				/*char * word;
 				scanf("%s", &word);*/
 
 				FILE* file = fopen(fileToOpen, "r");
 				if (file != NULL)
 				{
-					int * vector = malloc(fileLenghtI * sizeof(int));
-					int * vectorParallel = malloc(fileLenghtI * sizeof(int));
+					int * qs = malloc(fileLenghtI * sizeof(int));
+					int * eliminacao = malloc(fileLenghtI * sizeof(int));
 					int cur;
 					int i = 0;
 					do {
-						cur = fgetc(file);						
-						vector[i] = cur;
-						vectorParallel[i] = cur;
+						cur = fgetc(file);
+						qs[i] = eliminacao[i] = cur;
 						i++;
 					} while (i < fileLenghtI);
-					fclose(file);					
+					fclose(file);
+					/*
+										char * word;
+										scanf("%s", &word);*/
 
-					//printf("\nVetor inicial: ");
-					//for (i = 0; i < fileLenghtI; i++)
-					//	printf("%i,", vector[i]);
-					//printf("\n\n");
-					
-					ordena(fileLenghtI, 1, vector, vectorParallel);
+					int tam = ordena(fileLenghtI, 1, &qs);
+					tam = ordena(tam, 2, &eliminacao);
 
-			/*		printf("\nVetor paralelo processado: ");
-					for (i = 0; i < fileLenghtI; i++)
-						printf("%i,", vectorParallel[i]);
-					printf("\n\n");*/
-					
-					//processa.
-					free(vector);
-					vector = NULL;
+					if (eliminacao){
+						free(eliminacao);
+						eliminacao = NULL;
+					}
 
-					free(vectorParallel);
-					vectorParallel = NULL;
+					if (qs){
+						free(qs);
+						qs = NULL;
+					}
 				}
 				else
 					printf("\nArquivo não encontrado");
-		
+
 				/**/
-			}		
-		} 
+			}
+		}
 		else
 		{
 			closedir(dir);
 			break;
 		}
 	}
-	
+
 
 	printf("Works!");
 	char * word;
